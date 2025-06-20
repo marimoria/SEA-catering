181d742dbf4882852d995e49d4e8c8e853e8cd5d
marimoria <honiism@gmail.com>
2025-06-20 11:30:00 +0700

    Basic project structure

---
+++ main.js
@@ -0,0 +1,15 @@
+import { createApp } from "vue";
+import App from "./src/App.vue";
+import router from "./src/js/router";
+import "./src/js/vendor";
+
+createApp(App).use(router).mount("#app");
+
+function setVH() {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+}
+
+window.addEventListener("resize", setVH());
+window.addEventListener("orientationchange", setVH());
+setVH();
---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+    <router-view v-slot="{ Component }">
+        <Transition name="fade-slide" mode="out-in">
+            <component :is="Component" :viewport="viewport" :device="device" />
+        </Transition>
+    </router-view>
+</template>
+
+<script setup></script>
