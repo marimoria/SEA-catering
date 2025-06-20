663d78f5749aecc289e7bf788a368ea6a56cd368
marimoria <honiism@gmail.com>
2025-06-20 14:40:18 +0700

    Hero area development

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,30 @@
 <template>
-    <h1>Hi! This is home</h1>
+    <section class="home">
+        <navBar />
+        <div class="main_grid">
+            <div class="main_grid--hero_area">
+                <div class="hero_area--greeting_area">
+                    <p class="greeting_area--greet_text">
+                        Your favorite <span class="typing_text" id="animatedText"></span>
+                        <span class="cursor" style="color: black">|</span>
+                        <br />
+                        <span class="greeting_area--greet_text--second">Right at Your Door!</span>
+                    </p>
+                    <p class="greeting_area--slogan">Healthy Meals, Anytime, Anywhere</p>
+                </div>
+                <div class="hero_area--features_area"></div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <style scoped lang="scss">
     @use "../assets/styles/home.scss";
 </style>
 
-<script setup></script>
+<script setup>
+    import navBar from "../components/Navbar.vue";
+    import { onMounted } from "Vue";
+
+    onMounted(() => {});
+</script>
