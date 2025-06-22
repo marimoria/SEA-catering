e249e24e26ccbd027cd6f390274779eb0851f711
marimoria <honiism@gmail.com>
2025-06-22 14:50:16 +0700

    Added scrollto gsap plugin

--- src/js/vendor.js
+++ src/js/vendor.js
@@ -1,7 +1,8 @@
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { TextPlugin } from "gsap/TextPlugin";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 
-gsap.registerPlugin(ScrollTrigger, TextPlugin);
+gsap.registerPlugin(ScrollTrigger, TextPlugin, ScrollToPlugin);
 
-export { gsap, ScrollTrigger, TextPlugin };
+export { gsap, ScrollTrigger, TextPlugin, ScrollToPlugin };
