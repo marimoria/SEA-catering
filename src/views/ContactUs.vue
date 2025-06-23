ccce4165b51b5494dfb7ac3cac5654fa11e65472
marimoria <honiism@gmail.com>
2025-06-23 14:32:11 +0700

    Finished contact page

--- src/views/ContactUs.vue
+++ src/views/ContactUs.vue
@@ -1,29 +1,77 @@
 <template>
     <div class="contact">
+        <div class="background"></div>
         <Navbar />
-        <p class="title">Let's Get in Contact!</p>
+
+        <div class="contact_grid">
+            <p v-if="viewport.w >= 768" class="contact_grid--title">Reach Out Now!</p>
+
+            <div class="contact_grid--info">
+                <div class="contact_info--details">
+                    <p class="details--title">Contact Information</p>
+                    <p class="details--desc">
+                        This is the official contact information for SEA Catering. Feel free to
+                        shoot us a message if you have any issues with our service. Thank you for
+                        your patience.
+                    </p>
+                    <div class="details--grid">
+                        <div class="group">
+                            <img src="../assets/images/phone.svg" alt="" class="group--icon" />
+                            <p class="group--info">(+62) 123-456-789</p>
+                        </div>
+                        <div class="group">
+                            <img src="../assets/images/gmail.svg" alt="" class="group--icon" />
+                            <p class="group--info">SEACatering@support.com</p>
+                        </div>
+                        <div class="group">
+                            <img src="../assets/images/location.svg" alt="" class="group--icon" />
+                            <p class="group--info">Jakarta, Indonesia</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="contact_info--form">
+                    <form>
+                        <input type="text" placeholder="Your Name" />
+                        <input type="email" placeholder="Your Email" />
+                        <input type="text" placeholder="Subject" />
+                        <textarea placeholder="Your Message"></textarea>
+                        <button type="submit">Send Message</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+
         <img
-            id="hero_image_1"
             data-speedx="0.03"
             data-speedy="0.03"
-            class="parallax hero_image"
+            class="parallax bg_image--top_right"
+            src="../assets/images/chicken_platter.svg"
+            alt=""
+        />
+        <img
+            data-speedx="0.03"
+            data-speedy="0.03"
+            class="parallax bg_image--bottom_left"
+            src="../assets/images/pad_thai.svg"
+            alt=""
+        />
+
+        <!-- large screen only -->
+        <img
+            v-if="viewport.w >= 1024"
+            data-speedx="0.03"
+            data-speedy="0.03"
+            class="parallax bg_image--sec_right"
             src="../assets/images/protein_hero.svg"
             alt=""
         />
         <img
-            id="hero_image_2"
+            v-if="viewport.w >= 1024"
             data-speedx="0.03"
             data-speedy="0.03"
-            class="parallax hero_image"
+            class="parallax bg_image--sec_left"
             src="../assets/images/veggies_hero.svg"
-            alt=""
-        />
-        <img
-            id="hero_image_3"
-            data-speedx="0.03"
-            data-speedy="0.03"
-            class="parallax hero_image"
-            src="../assets/images/fruits_hero.svg"
             alt=""
         />
     </div>
@@ -36,55 +84,8 @@
 <script setup>
     import Navbar from "../components/Navbar.vue";
 
-    import { onMounted } from "vue";
-    import { gsap } from "../js/vendor.js";
-    import { useParallax } from "../components/composables/useParallax";
-
-    let currentIndex = 0;
-
-    function switchImage(nextIndex) {
-        const heroImages = Array.from(document.querySelectorAll(".hero_image"));
-        const current = heroImages[currentIndex];
-        const next = heroImages[nextIndex];
-
-        gsap.to(current, {
-            rotation: 360,
-            opacity: 0,
-            duration: 2,
-            ease: "power1.inOut",
-            onComplete: () => {
-                current.classList.remove("active");
-                gsap.set(current, { rotation: 0 });
-            }
-        });
-
-        gsap.to(next, {
-            rotation: 360,
-            opacity: 1,
-            duration: 2,
-            ease: "power1.inOut",
-            onStart: () => next.classList.add("active")
-        });
-
-        currentIndex = nextIndex;
-    }
-
-    onMounted(() => {
-        const heroImages = Array.from(document.querySelectorAll(".hero_image"));
-
-        heroImages[0].classList.add("active");
-        gsap.set(heroImages[0], { opacity: 1 });
-
-        const spinningTl = gsap.timeline({ repeat: -1 });
-
-        for (let i = 0; i < heroImages.length; i++) {
-            spinningTl.add(() => {
-                const nextIndex = (currentIndex + 1) % heroImages.length;
-                switchImage(nextIndex);
-            }, `+=2.5`);
-        }
-
-        const parallaxEls = document.querySelectorAll(".parallax");
-        parallaxEls.forEach((el) => useParallax(el));
+    const props = defineProps({
+        viewport: Object,
+        device: Object
     });
 </script>
