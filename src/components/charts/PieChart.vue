423a987cda025e8c0b64d969422b0cfb7e35e950
marimoria <honiism@gmail.com>
2025-07-01 11:13:23 +0700

    Installed vue-chartjs

---
+++ src/components/charts/PieChart.vue
@@ -0,0 +1,12 @@
+<template>
+    <Pie :options="chartOptions" :data="chartData" />
+</template>
+
+<script setup>
+    import { Pie } from "vue-chartjs";
+
+    defineProps({
+        chartData: Object,
+        chartOptions: Object
+    });
+</script>
